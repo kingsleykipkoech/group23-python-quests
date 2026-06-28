7f0e27677b09f42b6a2d3de19f5b355bac6ac7bc
mwnaikifelix-01 <f.mwaniki@alustudent.com>
2026-06-28 15:09:24 +0300

    quest 22

---
+++ quests/quest_22_personalized_scroll.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def personalized_greeting(name, quest):
+    print(f"welcome {name} and hope you find your quest which is {quest}")
+name = input("Enter your name: ")
+quest = input("What is your quest: ")
+    
+personalized_greeting(name, quest)
