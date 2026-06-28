2689aea0cb8d66d99a8eb3d5473eaf9ab5c79d3b
Kingsley Kipkoech <k.kipkoech@alustudent.com>
2026-06-28 11:37:33 +0200

    worked onQuest number 15  and 24

---
+++ quests/quest_15_nested_riddle.py
@@ -0,0 +1,11 @@
+print("Lets  start  the  riddle")
+choice =  input("Do  you  want  to  go  left or  right ? ")
+if choice == "left":
+	left = input("Do you  want to  swim or  wait? ")
+	if left ==  "swim":
+		print("You swam  and  found the sword  treausure,  nothing  can now  hurt you on land")
+	else:
+		print("You  delayed the  pirates took  the  treasure")
+else:
+	print("Game over you  have  remained on land and the hippo found  you")
+
