9d96addc33b4ffa4e51920676414b9a9f973b412
Lilian Kamikazi <l.kamikazi@alustudent.com>
2026-06-28 03:54:21 -0700

    Add quest 23 oracles vision

---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    return length * width
+
+area1 = calculate_area(5, 3)
+area2 = calculate_area(10, 4)
+print(f"Area of rectangle 1: {area1}")
+print(f"Area of rectangle 2: {area2}")
