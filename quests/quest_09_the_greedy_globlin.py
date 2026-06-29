e70c32df22ee35629f2adc91587f6ccfb8bca21c
gabrielagaba1 <g.agaba@alustudent.com>
2026-06-29 13:50:45 +0000

    created quest 9

---
+++ quests/quest_09_the_greedy_globlin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+gold = 27
+friends = 4
+
+each_friend_gets = gold // friends
+goblin_keeps = gold % friends
+
+print("Each friend gets:", each_friend_gets)
+print("The goblin keeps:", goblin_keeps)
